89d0dddd1220cae51735d78e41f18ab5ee661b4f
s.kiani <s.kiani@30S-KIANI>
2021-08-25 17:08:16 +0430

    init

---
+++ OrderPaymentService/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrderPaymentService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+        private readonly ILogger<OrderController> _logger;
+        private readonly IBus bus;
+
+        public OrderController(ILogger<OrderController> logger
+        ,IBus bus
+        )
+        {
+            _logger = logger;
+            this.bus = bus;
+        }
+
+        [HttpGet]
+        public Task CreateOrderAsync()
+        {
+            var rng = new Random();
+            bus.Publish<>
+
+            return Task.CompletedTask;
+        }
+    }
+}
