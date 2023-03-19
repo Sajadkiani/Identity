d8e61f55bf07fb50e12e3c080fc7ce4790edbe7b
s.kiani <sajadkiani9@gmail.com>
2023-03-19 08:36:38 +0330

    upgrade .net core 6

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -20,6 +20,8 @@ namespace ProductService
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    
+                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
